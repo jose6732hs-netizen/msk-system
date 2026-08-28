2b3d7edec64931502eee2212e498a157ff574e47
jose6732hs-netizen <jose6732hs@gmail.com>
2026-08-28 09:50:46 -0300

    fix: enable product image recovery globally

--- src/router.tsx
+++ src/router.tsx
@@ -1,6 +1,7 @@
 import { QueryClient } from "@tanstack/react-query";
 import { createRouter } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
+import "@/lib/product-image";
 
 const STALE_CHUNK_KEY = "msk_chunk_reload";
 
