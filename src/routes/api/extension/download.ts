9af83197a966d69869c5161c06e70dd76926b1a4
jose6732hs-netizen <jose6732hs@gmail.com>
2026-08-30 20:24:32 -0300

    security: protect extension download endpoint

--- src/routes/api/extension/download.ts
+++ src/routes/api/extension/download.ts
@@ -1,11 +1,12 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { extensionPreflight, handleExtensionDownload } from "@/lib/extension-telemetry.server";
+import { handleExtensionDownload } from "@/lib/extension-telemetry.server";
+import { enforceExtensionDeviceSecurity, extensionSecurityPreflight } from "@/lib/extension-device-security.server";
 
 export const Route = createFileRoute("/api/extension/download")({
   server: {
     handlers: {
-      OPTIONS: ({ request }) => extensionPreflight(request),
-      GET: ({ request }) => handleExtensionDownload(request),
+      OPTIONS: ({ request }) => extensionSecurityPreflight(request),
+      GET: async ({ request }) => (await enforceExtensionDeviceSecurity(request)) ?? handleExtensionDownload(request),
     },
   },
 });
