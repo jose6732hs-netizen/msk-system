a081ea980b993ac484c7b7d8c9fe8339fa634058
jose6732hs-netizen <jose6732hs@gmail.com>
2026-08-30 20:24:50 -0300

    security: protect extension GitHub download endpoint

--- src/routes/api/extension/github-download.ts
+++ src/routes/api/extension/github-download.ts
@@ -1,11 +1,12 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { githubDownloadPreflight, handleExtensionGithubDownload } from "@/lib/extension-github-download.server";
+import { handleExtensionGithubDownload } from "@/lib/extension-github-download.server";
+import { enforceExtensionDeviceSecurity, extensionSecurityPreflight } from "@/lib/extension-device-security.server";
 
 export const Route = createFileRoute("/api/extension/github-download")({
   server: {
     handlers: {
-      OPTIONS: ({ request }) => githubDownloadPreflight(request),
-      POST: ({ request }) => handleExtensionGithubDownload(request),
+      OPTIONS: ({ request }) => extensionSecurityPreflight(request),
+      POST: async ({ request }) => (await enforceExtensionDeviceSecurity(request)) ?? handleExtensionGithubDownload(request),
     },
   },
 });
